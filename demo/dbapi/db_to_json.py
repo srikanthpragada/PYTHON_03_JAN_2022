23c9854a2cef2e951cf9d4a89e3be294f873df3b
srikanthpragada <srikanthpragada@yahoo.com>
2022-02-17 08:00:01 +0530

    17-FEB

--- demo/dbapi/db_to_json.py
+++ demo/dbapi/db_to_json.py
@@ -16,7 +16,7 @@ cur = con.cursor()
 cur.execute("select * from employees")  # SQL Command
 
 for emp in cur.fetchall():
-    employees.append(emp_to_dict(emp))
+    employees.append(emp_to_dict(emp))  # Convert tuple to dict
 
 cur.close()
 con.close()
